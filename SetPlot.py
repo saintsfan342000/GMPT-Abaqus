fb18d4ff187be301ec2524bda70f6b5a701dd775
WRW_11 <filipp@utexas.edu>
2018-09-17 13:46:02 -0500

    SetPlot also exports data

--- SetPlot.py
+++ SetPlot.py
@@ -10,13 +10,58 @@ import os
 Compare the strain paths, sts-stns, and ur_profs
 '''
 
-expts = [11, 4, 2, 8, 3, 12]
-jobno = [2, 1, 2, 6, 2, 1]
+try:
+    case = argv[1]
+except IndexError:
+    case = 'BestSoFar'
+
+if case.upper() in ['BAD']:
+    expts = [11, 4, 2, 8, 3, 12]
+    jobno = [1 for i in range(len(expts))]
+elif case.upper() in ['1PCT']:
+    expts = [11, 4, 2, 8, 3, 12]
+    jobno = [3, 3, 3, 7, 4, 3]
+else:
+    expts = [11, 4, 2, 8, 3, 12]
+    jobno = [2, 2, 2, 6, 3, 2]
+
+export = True
+
+if export:
+    import pandas as pd
+    fname = 'GMPT-{}.xlsx'.format(case)
+    fid = pd.ExcelWriter(fname)
+    exphead = 'Expt AxSts, HoopSts, AxStn, HoopStn'.split(', ')
+    simhead = 'Sim AxSts, HoopSts, AxStn, HoopStn'.split(', ')
+    LL = n.empty((len(expts), 10))
+    FA = n.empty((len(expts), 10))
+    
 key = n.genfromtxt('ExptSummary.dat', delimiter=',')
 key = key[ n.in1d(key[:,0],expts) ]
 key = key[ n.argsort(key[:,4]) ]
 
 repl = {'88':'11', '99':'8', '00':'8'}
+
+p.style.use('mysty-quad')
+p.rcParams['axes.labelsize'] = 18
+p.rcParams['lines.markersize'] = 3
+pad = .9
+hgap = 1.25
+vgap = 2
+axwt = 10/3
+axht = 8/3
+W = 2*pad + hgap + 2*axwt
+H = 2*pad + vgap + 2*axht
+fig = p.figure(figsize=(W,H))
+# Top left
+ax1 = fig.add_axes([pad/W, (pad+vgap+axht)/H, axwt/W, axht/H])
+# Top right
+ax2 = fig.add_axes([(pad+hgap+axwt)/W, (pad+vgap+axht)/H, axwt/W, axht/H])
+# Bottom left
+ax3 = fig.add_axes([pad/W, (pad+vgap/3-1/3)/H, axwt/W, (axht+2/3)/H])
+# Bottom right
+ax4 = fig.add_axes([(pad+hgap+axwt)/W, .8*pad/H, axwt/W, 1.5*axht/H])
+
 
 for k, (exp,job) in enumerate(zip(expts, jobno)):
 
@@ -58,12 +103,12 @@ for k, (exp,job) in enumerate(zip(expts, jobno)):
     key = key[ key[:,0] == int(exp) ].ravel()
     Ro, to = key[6:8]
 
-
     # Simulation data
-    cutoff = n.genfromtxt('{}/Cutoff.dat'.format(simpath),dtype=int)
+    cutoff = n.genfromtxt('{}/Cutoff.dat'.format(simpath),dtype=int, delimiter=',')[0]
     d = n.genfromtxt('{}/{}_NewResults.dat'.format(simpath, exp), delimiter=',')
     if cutoff > 0:
         d = d[:cutoff+1]
+        print(exp, cutoff)
     #if exp == 8:
     #    d[:,[5,6,12]]*=1.8
     #d[:,3:5]+=1
@@ -89,6 +134,9 @@ for k, (exp,job) in enumerate(zip(expts, jobno)):
     # d[:,1:3] = n.log(1+d[:,1:3])
     xd[:,1:]*=100
     xstn= n.genfromtxt('{}/WholeFieldAverage.dat'.format(exppath), delimiter=',', usecols=(1,2))
+    #From Cal data!
+    # [0]Stage, [1]Wp, [2]SigX_Tru, [3]SigQ_True, [4]ex, [5]eq, [6]e3, [7]ep_x, [8]ep_q, [9]ep_3, [10]R_tru, [11]th_tru
+    #xstn = n.genfromtxt('{}/CalData.dat'.format(calpath), delimiter=',', usecols=(4,5))
     # Expt nominal hoop stn in area close to ES_ANALZONE
     xstn = n.log(1+xstn)*100
     xex, xeq = xstn.T
@@ -99,52 +147,33 @@ for k, (exp,job) in enumerate(zip(expts, jobno)):
     xu[:,1:]*=100
     # Limitload y-coord, ur/Ro
 
+    
     alpha, blank = n.polyfit(d[:,4],d[:,3],1)
-    annotstr = '{} || {:.2f}'.format(exp,alpha)
-
-    if k == 0:
-        p.style.use('mysty-quad')
-        p.rcParams['axes.labelsize'] = 18
-        p.rcParams['lines.markersize'] = 3
-        pad = .9
-        hgap = 1.25
-        vgap = 2
-        axwt = 10/3
-        axht = 8/3
-        W = 2*pad + hgap + 2*axwt
-        H = 2*pad + vgap + 2*axht
-        fig = p.figure(figsize=(W,H))
-        # Top left
-        ax1 = fig.add_axes([pad/W, (pad+vgap+axht)/H, axwt/W, axht/H])
-        # Top right
-        ax2 = fig.add_axes([(pad+hgap+axwt)/W, (pad+vgap+axht)/H, axwt/W, axht/H])
-        # Bottom left
-        ax3 = fig.add_axes([pad/W, (pad+vgap/3-1/3)/H, axwt/W, (axht+2/3)/H])
-        # Bottom right
-        ax4 = fig.add_axes([(pad+hgap+axwt)/W, .8*pad/H, axwt/W, 1.5*axht/H])
+    annotstr = '{}/{} || {:.2f}'.format(exp,job,alpha)
 
     # ax1: Ax sts vs ax stn.  
     # delta/L
-    a, = ax1.plot(d[:,5],d[:,3], '--', label=annotstr)
-    x,= ax1.plot(xd[:,4], xst[:,4], a.get_color())
+    a, = ax1.plot(d[:,5],d[:,3], ':', label=annotstr)
+    x,= ax1.plot(xd[:,4], xst[:,4], a.get_color(), alpha=0.5)
     ax1.plot(d[simloc,5],d[simloc,3], 'rD')
     ax1.plot(xd[exploc,4], xst[exploc,4], 'rD')
     #The point field averages
-    #ax1.plot(d[:,11],d[:,2], '--', color=simcolor)
-    #ax1.plot(n.log(xd[:,1]*.01+1)*100, xst[:,4], '--', color=expcolor)
+    #ax1.plot(d[:,11],d[:,2], ':', color=simcolor)
+    #ax1.plot(n.log(xd[:,1]*.01+1)*100, xst[:,4], ':', color=expcolor)
     # Labels and annotations
     if k == len(expts) - 1:
         ax1.set_xlabel('$\\delta/\\mathsf{L}$ (%)')
         ax1.set_ylabel('$\\Sigma_\\mathsf{x}$\n(ksi)')
         ax1.axis(ymin=0, xmin=-3)
-        f.ezlegend(ax1, title='Exp||$\\eta\\prime$', loc='lower right')
+        f.ezlegend(ax1, title='Exp/#  ||  $\\eta\\prime$', loc='lower right')
         f.eztext(ax1, '$\\mathsf{L}_\\mathsf{g}$=1"', 'ul')
         f.myax(ax1)
 
+        
     # ax2: Hoop sts vs Hoop stn
     # e_q
-    ax2.plot(d[:,11],d[:,4], '--', label=annotstr)
-    ax2.plot(xeq, xst[:,5], a.get_color())
+    ax2.plot(d[:,11],d[:,4], ':', label=annotstr)
+    ax2.plot(xeq, xst[:,5], a.get_color(), alpha=0.5)
     ax2.plot(d[simloc,11], d[simloc,4], 'rD')
     ax2.plot(xeq[exploc], xst[exploc,5], 'rD')
     if k == len(expts) - 1:
@@ -155,19 +184,9 @@ for k, (exp,job) in enumerate(zip(expts, jobno)):
         f.eztext(ax2, 'Avg. of Pts.', 'br')
         f.myax(ax2)
 
-    #[0] Force (kip), [1]Pressure (ksi), [2]Vol, [3]NomAxSts, [4]NomHoopSts, 
-    # [5]d/Lg lo, [6]d/Lg Back, [7,8,9]S11,22,33, [10,11,12]LE11,22,33, 
-    # [13]PEEQ(SDV1), [14]EqSts(SDV2)
-    
-    #xex, xeq = n.genfromtxt('{}/CalData.dat'.format(calpath),
-    #                        delimiter=',', usecols=(7,8), unpack=1)*100
-
-    #d[:,12] -= (d[:,9]-0.32*d[:,8])/10036
-    #d[:,11] -= (d[:,8]-0.32*d[:,9])/10036
-
     # ax3: Axial vs hoop stn
-    ax3.plot(d[:,11], d[:,12], '--', label=annotstr)
-    ax3.plot(xeq, xex, a.get_color())
+    ax3.plot(d[:,11], d[:,12], ':', label=annotstr)
+    ax3.plot(xeq, xex, a.get_color(), alpha=0.5)
     ax3.plot(d[simloc,11], d[simloc, 12], 'rD')
     ax3.plot(xeq[exploc], xex[exploc], 'rD')
     if k == len(expts) - 1:
@@ -178,15 +197,49 @@ for k, (exp,job) in enumerate(zip(expts, jobno)):
         f.myax(ax3)
 
     # Ax4: Ur Profiles
-    ax4.plot(100*u[:,(simloc+2)]/u[:,1], u[:,0]*2/4, '--', label=annotstr)
-    ax4.plot(xu[:,1], 2*xu[:,0]/4, a.get_color())
+    ax4.plot(100*u[:,(simloc+2)]/u[:,1], u[:,0]*2/4, ':', label=annotstr)
+    ax4.plot(xu[:,1], 2*xu[:,0]/4, a.get_color(), alpha=0.5)
     if k == len(expts) - 1:
         ax4.set_xlabel('u$_\\mathsf{r}$/R$_\\mathsf{o}$ (%)')
         ax4.set_ylabel('$\\frac{\\mathsf{2y}_\\mathsf{o}}{\\mathsf{L}_\\mathsf{g}}$')
         ax4.axis(xmin=0,ymin=-1,ymax=1)
         f.eztext(ax4, 'LL', 'br')
         f.myax(ax4, autoscale=.75, nudge=('left',.2,0))
+    
+    if export:
+        expdata = n.c_[xst[:,4:6], xex, xeq]
+        simdata = n.c_[d[:,3:5], d[:,12], d[:,11]]
+        LL[k,:2] = exp, alpha
+        LL[k,2:6] = expdata[exploc]
+        LL[k,6:] = simdata[simloc]
+        FA[k,:2] = exp, alpha
+        FA[k, 2:6] = expdata[-1]
+        FA[k, 6:] = simdata[-1]
+        pd.DataFrame(expdata).to_excel(fid, sheet_name='GMPT-{}'.format(exp), 
+                    header=exphead, index=False)
+        pd.DataFrame(simdata).to_excel(fid, sheet_name='GMPT-{}'.format(exp), 
+                    header=simhead, index=False, startcol=(expdata.shape[1]+2))
+               
 
-fig.savefig('AllPlot.jpg', dpi=200, bbox_inches='tight')
+if export:
+    pd.DataFrame(LL).to_excel(fid, sheet_name='LL', header=['Exp', 'Alpha']+exphead+simhead, index=False)
+    pd.DataFrame(FA).to_excel(fid, sheet_name='Failure', header=['Exp', 'Alpha']+exphead+simhead, index=False)
+    fid.close()
+              
+fig.suptitle(case)
+fig.savefig('AllPlot_{}.jpg'.format(case), dpi=200, bbox_inches='tight')
+fig.savefig('AllPlot_{}.pdf'.format(case),bbox_inches='tight')
 
 p.show('all')
+
+if export:
+    fig, ax1, ax2 = f.make12()
+    l, = ax1.plot(expdata[:,3], expdata[:,1], alpha=0.5)
+    ax1.plot(simdata[:,3], simdata[:,1], '--', color=l.get_color())
+    ax1.plot(LL[-1,5], LL[-1, 3], 'rD')
+    ax1.plot(LL[-1,5+4], LL[-1, 3+4], 'ro')
+    l, = ax2.plot(expdata[:,2], expdata[:,0], alpha=0.5)
+    ax2.plot(simdata[:,2], simdata[:,0], '--', color=l.get_color())
+    ax2.plot(LL[-1,4], LL[-1, 2], 'rD')
+    ax2.plot(LL[-1,4+4], LL[-1, 2+4], 'ro')    
+    p.show()
